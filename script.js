42dfce978b55d5bdf4cec5446211ae8ce4925196
Daniel Dário <113317041+DaniellDario@users.noreply.github.com>
2023-09-23 15:25:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  // Troca de imagem
+
+  let img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "media/avatar-lightmode.png")
+  } else {
+    img.setAttribute("src", "media/avatar.png")
+  }
+
+}
